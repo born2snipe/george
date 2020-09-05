9ee721544468456bea2a1dffd98744848a5fec75
Dan Dudley <born2snipe@gmail.com>
2020-09-05 17:33:22 -0400

    hooked up the library to actually update the audio file...no clue if this works

--- src/main/java/george/multialbum/UpdateTrackNumberInMetaData.java
+++ src/main/java/george/multialbum/UpdateTrackNumberInMetaData.java
@@ -1,9 +1,21 @@
 package george.multialbum;
 
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+
 import java.io.File;
 
 public class UpdateTrackNumberInMetaData {
     public void update(File audioFile, String trackNumber) {
-
+        try {
+            AudioFile f = AudioFileIO.read(audioFile);
+            Tag tag = f.getTag();
+            tag.setField(FieldKey.TRACK, trackNumber);
+            f.commit();
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed updating track number (" + trackNumber + ") for: " + audioFile.getName(), e);
+        }
     }
 }
